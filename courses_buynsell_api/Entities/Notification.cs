88e3f7b5de7c034786503979d070496867e9426a
SA-NG-OC <sang22102005@gmail.com>
2025-10-25 17:43:13 +0700

    Initial commit

---
+++ courses_buynsell_api/Entities/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace courses_buynsell_api.Entities;
+
+public class Notification
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Message { get; set; } = string.Empty;
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    public bool IsRead { get; set; }
+    public int SellerId { get; set; }
+    public User? Seller { get; set; }
+}
---
+++ courses_buynsell_api/Entities/TransactionDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace courses_buynsell_api.Entities;
+
+public class TransactionDetail
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required] 
+    public decimal Price { get; set; }
+    [Required] 
+    public int TransactionId { get; set; }
+    public Transaction? Transaction { get; set; }
+    [Required] 
+    public int CourseId { get; set; }
+    public Course? Course { get; set; }
+}
