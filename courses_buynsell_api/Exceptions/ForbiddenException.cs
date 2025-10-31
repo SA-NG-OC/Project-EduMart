f31c0978d2674eddab256417b34cf7bfc1b53ef4
SA-NG-OC <sang22102005@gmail.com>
2025-10-31 21:25:56 +0700

    fix: update DB and complete auth

---
+++ courses_buynsell_api/Exceptions/ForbiddenException.cs
@@ -0,0 +1,7 @@
+// File: Exceptions/ForbiddenException.cs
+namespace courses_buynsell_api.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException(string message) : base(message) { }
+}
---
+++ courses_buynsell_api/Migrations/20251031142032_updateDB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace courses_buynsell_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Courses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Courses");
+        }
+    }
+}
