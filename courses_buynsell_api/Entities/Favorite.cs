eed47daf807d98bbe2fe9e76e8e0bdd306225d05
SA-NG-OC <sang22102005@gmail.com>
2025-11-01 18:12:03 +0700

    update database favorite

--- courses_buynsell_api/Entities/Favorite.cs
+++ courses_buynsell_api/Entities/Favorite.cs
@@ -1,16 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace courses_buynsell_api.Entities;
+﻿namespace courses_buynsell_api.Entities;
 
 public class Favorite
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-
     public int UserId { get; set; }
     public User? User { get; set; }
-    public Course? Course { get; set; }
+
     public int CourseId { get; set; }
+    public Course? Course { get; set; }
 }
